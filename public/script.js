3f8e0629009988527df6c0dab3b0e1d611ab62ae
Mangesh Kumar <titancoder@hotmail.com>
2020-07-12 19:46:17 +0530

    Add Login, SignUp & Forgot Password form

--- public/script.js
+++ public/script.js
@@ -7,3 +7,55 @@ const toggle = () => {
 		toggleMenu.classList.replace("d-block", "d-none");
 	}
 };
+
+const modal = document.getElementById("modal");
+
+const openLoginForm = () => {
+	modal.style.display = "block";
+	//modal.classList.toggle("hide");
+};
+
+const closeModal = (e) => {
+	if (e.target == modal) {
+		modalForgotPassword.style.display = "none";
+		modalLoginSignUp.style.display = "flex";
+		login_form.style.display = "block";
+		signup_form.style.display = "none";
+		login_label.click();
+		modal.style.display = "none";
+	}
+};
+
+window.addEventListener("click", closeModal);
+
+const login_label = document.getElementById("login_label");
+const signup_label = document.getElementById("signup_label");
+const login_form = document.getElementById("login_form");
+const signup_form = document.getElementById("signup_form");
+
+login_label.addEventListener("click", () => {
+	login_form.style.display = "block";
+	signup_form.style.display = "none";
+});
+
+signup_label.addEventListener("click", () => {
+	signup_form.style.display = "block";
+	login_form.style.display = "none";
+});
+
+const modalLoginSignUp = document.getElementById("modal_content");
+const modalForgotPassword = document.getElementById("modal_content_fp");
+
+const activateLoginSignUpForm = (e) => {
+	modalForgotPassword.style.display = "none";
+	modalLoginSignUp.style.display = "flex";
+	//modalForgotPassword.classList.replace("hide", "d-flex");
+	//modalLoginSignUp.classList.replace("d-flex", "hide");
+};
+
+const activateForgotPasswordForm = (e) => {
+	modalLoginSignUp.style.display = "none";
+	modalForgotPassword.style.display = "flex";
+	//modalLoginSignUp.classList.replace("hide", "d-flex");
+	//modalForgotPassword.classList.replace("d-flex", "hide");
+};
